ab46fefe8c0b279e94cce5b77ba137f90513a9f1
dthornewell <dthornew@purdue.edu>
2024-09-17 23:57:16 -0400

    added grid based optimizations

--- static/particles.js
+++ static/particles.js
@@ -1,6 +1,34 @@
-var buffer = 30;
-var radius = 100;
-var rad2 = radius*radius;
+const buffer = 30;
+const radius = 100;
+
+class Section {
+    constructor(left, top, width, height) {
+        this.left = left;
+        this.top = top;
+        this.right = left + width;
+        this.bottom = top + height;
+        this.particles = new Set();
+    }
+
+    addParticle(particle) {
+        this.particles.add(particle);
+    }
+
+    removeParticle(particle) {
+        this.particles.delete(particle);
+    }
+
+    updateParticles() {
+        this.particles.forEach(particle => {
+            particle.update();
+            particle.draw();
+        });
+    }
+
+    exportParticles() {
+        return [...this.particles];
+    }
+}
 
 class Particle {
     constructor(particleNetwork) {
@@ -45,62 +73,35 @@ class Particle {
 
         // Draw the chevron/arrow
         this.context.beginPath();
-        this.context.moveTo(-5, -3); // Back left point
-        this.context.lineTo(5, 0);  // Front point
-        this.context.lineTo(-5, 3);  // Back right point
+        this.context.moveTo(-2.5, -1.5); // Back left point
+        this.context.lineTo(2.5, 0);  // Front point
+        this.context.lineTo(-2.5, 1.5);  // Back right point
         this.context.closePath();
 
         this.context.fillStyle = this.particleColor;
-        this.context.globalAlpha = 1;
         this.context.fill();
-
         this.context.restore();
     }
 }
-
+   
 class ParticleNetwork {
     constructor(container, options = {}) {
         this.container = container;
-        this.containerSize = {
-            width: this.container.offsetWidth,
-            height: this.container.offsetHeight
-        };
-        this.options = {
-            particleColor: options.particleColor || '#fff',
-            background: options.background || '#1a252f',
-            interactive: options.interactive !== undefined ? options.interactive : true,
-            velocity: options.speed,
-            density: options.density,
-            numSections: options.numSections !== undefined ? options.numSections : 1
-        };
-
+        this.containerSize = { width: container.offsetWidth, height: container.offsetHeight };
+        this.options = Object.assign({
+            particleColor: '#fff',
+            background: '#1a252f',
+            interactive: true,
+            velocity: 1,
+            density: 4000,
+            numSections: 5,
+        }, options);
+        this.sections = [];
         this.init();
     }
 
     init() {
-        // Create background
-        this.backgroundElement = document.createElement('div');
-        this.container.appendChild(this.backgroundElement);
-        this.setStyles(this.backgroundElement, {
-            position: 'absolute',
-            top: 0,
-            left: 0,
-            bottom: 0,
-            right: 0,
-            zIndex: 1
-        });
-
-        if (/^#[0-9A-F]{3,6}$/i.test(this.options.background)) {
-            this.setStyles(this.backgroundElement, { background: this.options.background });
-        } else if (/\.(gif|jpg|jpeg|tiff|png)$/i.test(this.options.background)) {
-            this.setStyles(this.backgroundElement, {
-                background: `url("${this.options.background}") no-repeat center`,
-                backgroundSize: 'cover'
-            });
-        } else {
-            console.error('Invalid background image or color');
-            return;
-        }
+        this.createBackground();
 
         // Create canvas
         this.canvas = document.createElement('canvas');
@@ -114,15 +115,21 @@ class ParticleNetwork {
 
         window.addEventListener('resize', this.handleResize.bind(this));
 
-        this.particles = [];
+        const sectionWidth = Math.ceil((this.canvas.width + 2 * buffer) / this.options.numSections);
+        const sectionHeight = Math.ceil((this.canvas.height + 2 * buffer) / this.options.numSections);
+        this.createSections(sectionWidth, sectionHeight);
+
+        // Create particles
         for (let i = 0; i < this.canvas.width * this.canvas.height / this.options.density; i++) {
-            this.particles.push(new Particle(this));
+            //Add particle to section
+            let particle = new Particle(this);
+            let section = this.sections[((particle.y - buffer) / sectionHeight) | 0][((particle.x - buffer) / sectionWidth) | 0];
+            section.addParticle(particle);
         }
 
         if (this.options.interactive) {
             this.interactiveParticle = new Particle(this);
             this.interactiveParticle.velocity = { x: 0, y: 0 };
-            this.particles.push(this.interactiveParticle);
 
             this.canvas.addEventListener('mousemove', this.handleMouseMove.bind(this));
             this.canvas.addEventListener('mouseup', this.handleMouseUp.bind(this));
@@ -131,17 +138,80 @@ class ParticleNetwork {
         requestAnimationFrame(this.update.bind(this));
     }
 
+    createBackground() {
+        this.backgroundElement = document.createElement('div');
+        this.container.appendChild(this.backgroundElement);
+        this.setStyles(this.backgroundElement, {
+            position: 'absolute',
+            top: 0, left: 0, bottom: 0, right: 0, zIndex: 1,
+        });
+        if (/^#[0-9A-F]{3,6}$/i.test(this.options.background)) {
+            this.backgroundElement.style.background = this.options.background;
+        }
+    }
+
+    createSections(sectionWidth, sectionHeight) {        
+        this.sections = Array.from({ length: this.options.numSections }, (_, i) => 
+            Array.from({ length: this.options.numSections }, (_, j) => {
+                const left = -buffer + j * sectionWidth;
+                const top = -buffer + i * sectionHeight;
+                const width = (j === this.options.numSections - 1) ? this.canvas.width + buffer - j * sectionWidth : sectionWidth;
+                const height = (i === this.options.numSections - 1) ? this.canvas.height + buffer - i * sectionHeight : sectionHeight;
+                return new Section(left, top, width, height);
+            })
+        );
+    }
+
     setStyles(element, styles) {
         for (let property in styles) {
             element.style[property] = styles[property];
         }
     }
 
-    handleResize() {  
-        // Update the canvas dimensions
-        this.canvas.width = this.containerSize.width = this.container.offsetWidth;
-        this.canvas.height = this.containerSize.height = this.container.offsetHeight;
-    }
+
+    handleResize() {
+        // Update canvas and container sizes
+        const { offsetWidth, offsetHeight } = this.container;
+        this.canvas.width = this.containerSize.width = offsetWidth;
+        this.canvas.height = this.containerSize.height = offsetHeight;
+    
+        //Get all particles
+        let particles = [];
+        for (let i = 0; i < this.options.numSections; i++) {
+            for (let j = 0; j < this.options.numSections; j++) {
+                particles.push(...this.sections[i][j].exportParticles());
+            }
+        }
+    
+        // Reinitialize sections based on new canvas size
+        this.sections = [];
+        const sectionWidth = Math.ceil((this.canvas.width + 2 * buffer) / this.options.numSections);
+        const sectionHeight = Math.ceil((this.canvas.height + 2 * buffer) / this.options.numSections);
+    
+        for (let i = 0; i < this.options.numSections; i++) {
+            this.sections[i] = [];
+            const top = -buffer + i * sectionHeight;
+            const height = (i === this.options.numSections - 1) 
+                ? this.canvas.height + buffer - i * sectionHeight 
+                : sectionHeight;
+    
+            for (let j = 0; j < this.options.numSections; j++) {
+                const left = -buffer + j * sectionWidth;
+                const width = (j === this.options.numSections - 1) 
+                    ? this.canvas.width + buffer - j * sectionWidth 
+                    : sectionWidth;
+    
+                this.sections[i][j] = new Section(left, top, width, height);
+            }
+        }
+    
+        //Reassign particles to sections
+        for (let particle of particles) {
+            particle.update();
+            let section = this.sections[((particle.y - buffer) / sectionHeight) | 0][((particle.x - buffer) / sectionWidth) | 0];
+            section.addParticle(particle);
+        }
+    }    
 
     handleMouseMove(event) {
         this.interactiveParticle.x = event.clientX - this.canvas.offsetLeft;
@@ -149,48 +219,172 @@ class ParticleNetwork {
     }
 
     handleMouseUp() {
+        var sectionWidth = ((this.canvas.width + 2 * buffer)/ this.options.numSections) | 0;
+        var sectionHeight = ((this.canvas.height + 2 * buffer)/ this.options.numSections) | 0;
+        //Assign velocity to interactive particle
         this.interactiveParticle.velocity = {
             x: (Math.random() - 0.5) * this.options.velocity,
             y: (Math.random() - 0.5) * this.options.velocity
         };
+        //Add particle to section
+        let section = this.sections[((this.interactiveParticle.y - buffer) / sectionHeight) | 0][((this.interactiveParticle.x - buffer) / sectionWidth) | 0];
+        section.addParticle(this.interactiveParticle);
+
+        //Create new interactive particle
         this.interactiveParticle = new Particle(this);
         this.interactiveParticle.velocity = { x: 0, y: 0 };
-        this.particles.push(this.interactiveParticle);
-        console.log(this.particles.length);
+    }
+
+    drawSectionBoundaries() {
+        this.context.strokeStyle = '#FF0000'; // Red lines for visibility
+        this.context.lineWidth = 1;
+
+        var numSections = this.options.numSections;
+        for (let i = 0; i < numSections; i++) {
+            for (let j = 0; j < numSections; j++) {
+                let section = this.sections[i][j];
+                // Draw section boundary
+                this.context.beginPath();
+                this.context.rect(section.left + buffer, section.top + buffer, 
+                                  section.right - section.left, section.bottom - section.top);
+                this.context.stroke();
+            }
+        }
     }
 
     update() {
-        //const currentTime = performance.now();
-
+        const currentTime = performance.now();
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
-        this.context.globalAlpha = 1;
 
-        for (let i = 0; i < this.particles.length; i++) {
-            const particle = this.particles[i];
-            particle.update();
-            particle.draw();
+        this.drawSectionBoundaries();
 
-            for (let j = this.particles.length - 1; j > i; j--) {
-                const otherParticle = this.particles[j];
+        //Update particles
+        var numSections = this.options.numSections;
+        this.sections.flat().forEach(section => section.updateParticles());
 
-                if (Math.abs(particle.x - otherParticle.x) > radius || Math.abs(particle.y - otherParticle.y) > radius) {
-                    continue;
+        //Move particles to correct sections
+        for (let i = 0; i < numSections; i++) {
+            for (let j = 0; j < numSections; j++) {
+                var section = this.sections[i][j];
+                let particles = section.exportParticles();
+                for (let particle of particles) {
+                    //Check if particle is in bounds
+                    if (particle.x > section.left && particle.x < section.right && particle.y > section.top && particle.y < section.bottom) {
+                        continue;
+                    }
+
+                    //Check against neighbors
+                    for (let k = -1; k <= 1; k++) {
+                        for (let l = -1; l <= 1; l++) {
+                            if (k == 0 && l == 0) {
+                                continue;
+                            }
+                            //Get neighbor (including wrap around sections)
+                            let neighbor = this.sections[(i + k + numSections) % numSections][(j + l + numSections) % numSections];
+                            if (neighbor.left <= particle.x && particle.x <= neighbor.right && neighbor.top <= particle.y && particle.y <= neighbor.bottom) {
+                                this.sections[i][j].removeParticle(particle);
+                                neighbor.addParticle(particle);
+                                break;
+                            }
+                        }
+                    }
                 }
 
-                const distance = (Math.pow(particle.x - otherParticle.x, 2) +
-                                           Math.pow(particle.y - otherParticle.y, 2));
+            }
+        }
 
-                if (distance <= rad2) {
-                    this.context.beginPath();
-                    this.context.strokeStyle = this.options.particleColor;
-                    //this.context.globalAlpha = (rad2 - distance) / rad2;
-                    this.context.lineWidth = (rad2 - distance) / (rad2);
-                    this.context.moveTo(particle.x, particle.y);
-                    this.context.lineTo(otherParticle.x, otherParticle.y);
-                    this.context.stroke();
+        //Draw connections between particles
+        for (let i = 0; i < numSections; i++) {
+            for (let j = 0; j < numSections; j++) {
+                var section = this.sections[i][j];
+                let particles1 = this.sections[i][j].exportParticles();
+                
+                //Check against self 
+                for (let i = 0; i < particles1.length; i++) {
+                    const particle1 = particles1[i];
+                    for (let j = particles1.length - 1; j > i; j--) {
+                        const particle2 = particles1[j];
+                        let xDiff = Math.abs(particle1.x - particle2.x);
+                        let yDiff = Math.abs(particle1.y - particle2.y);
+                        if (xDiff > radius || yDiff > radius) {
+                            continue;
+                        }
+                        let distance = xDiff*xDiff + yDiff*yDiff;
+                        if (distance <= radius*radius) {
+                            this.context.beginPath();
+                            this.context.strokeStyle = this.options.particleColor;
+                            this.context.lineWidth = (radius*radius - distance) / (radius*radius);
+                            this.context.moveTo(particle1.x, particle1.y);
+                            this.context.lineTo(particle2.x, particle2.y);
+                            this.context.stroke();
+                        }
+                    }
+                }
+
+                
+                //Assign neighbors: nw, n, ne, and e (if not on edge)
+                var toCheck = [];
+                if (i > 0) {
+                    toCheck.push(this.sections[i-1][j]);
+                    if (j > 0) {
+                        toCheck.push(this.sections[i-1][j-1]);
+                    }
+                    if (j < numSections - 1) {
+                        toCheck.push(this.sections[i-1][j+1]);
+                    }
+                }
+                if (j < numSections - 1) {
+                    toCheck.push(this.sections[i][j+1]);
+                }
+                //Check against neighbors
+                for (let neighbor of toCheck) {
+                    let particles2 = neighbor.exportParticles();
+                    for (let particle1 of particles1) {
+                        for (let particle2 of particles2) {
+                            let xDiff = Math.abs(particle1.x - particle2.x);
+                            let yDiff = Math.abs(particle1.y - particle2.y);
+                            if (xDiff > radius || yDiff > radius) {
+                                continue;
+                            }
+                            let distance = xDiff*xDiff + yDiff*yDiff;
+                            if (distance <= radius*radius) {
+                                this.context.beginPath();
+                                this.context.strokeStyle = this.options.particleColor;
+                                this.context.lineWidth = (radius*radius - distance) / (radius*radius);
+                                this.context.moveTo(particle1.x, particle1.y);
+                                this.context.lineTo(particle2.x, particle2.y);
+                                this.context.stroke();
+                            }
+                        }
+                    }
+                }
+
+                //Check against interactive particle
+                if (this.options.interactive) {
+                    let particle2 = this.interactiveParticle;
+                    particle2.update();
+                    particle2.draw();
+                    for (let particle1 of particles1) {
+                        let xDiff = Math.abs(particle1.x - particle2.x);
+                        let yDiff = Math.abs(particle1.y - particle2.y);
+                        if (xDiff > radius || yDiff > radius) {
+                            continue;
+                        }
+                        let distance = xDiff*xDiff + yDiff*yDiff;
+                        if (distance <= radius*radius) {
+                            this.context.beginPath();
+                            this.context.strokeStyle = this.options.particleColor;
+                            this.context.lineWidth = (radius*radius - distance) / (radius*radius);
+                            this.context.moveTo(particle1.x, particle1.y);
+                            this.context.lineTo(particle2.x, particle2.y);
+                            this.context.stroke();
+                        }
+                    }
                 }
             }
         }
+
+        //Animate frame
         if (this.options.velocity !== 0) {
             requestAnimationFrame(this.update.bind(this));
         }
